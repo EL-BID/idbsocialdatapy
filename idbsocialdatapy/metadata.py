105ea44c04c790c4c6442a39bcc8373b8c94e222
Maria Reyes Retana <mariareyesretana94@gmail.com>
2022-11-14 10:58:06 -0500

    feat: add query_dictionaty to metadata

--- idbsocialdatapy/metadata.py
+++ idbsocialdatapy/metadata.py
@@ -20,3 +20,17 @@ def get_themes():
     get_themes_url = os.path.join(url_links["metadata_url"], "themes")
     df = pd.read_json(get_themes_url)
     return df
+
+
+def query_dictionary(indicator='All'):
+    """
+    Returns dictionary as a pandas dataframe
+    """
+    url_links =  iadburls()
+    get_dictionary_url = os.path.join(url_links["metadata_url"], "dictionary?")
+    
+    if indicator != 'All': get_dictionary_url = get_dictionary_url + "&indicator=" + indicator
+    
+    df = pd.read_json(get_dictionary_url)
+    return df
+        
