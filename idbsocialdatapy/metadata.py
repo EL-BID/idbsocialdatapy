8e0282a726c461d037184e6b2602349d93f312fd
Genrry Hernandez <genrryhernandez@gmail.com>
2022-11-14 16:45:08 -0500

    fix: add new external imports and fix existing ones to use the correct scope

--- idbsocialdatapy/metadata.py
+++ idbsocialdatapy/metadata.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 
-from idbsocialdatapy import iadburls
+from idbsocialdatapy.utils import iadburls
 
 
 def get_countries():
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from idbsocialdatapy.utils import iadburls
+from idbsocialdatapy import iadburls
 
 
 @pytest.fixture
